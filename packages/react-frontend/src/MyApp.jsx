5cccbff6b28bd6bd4359f2ad996ecc0ad95a4025
JarekOrta <jjorta@calpoly.edu>
2026-09-14 23:30:28 -0700

    Connect frontend form to backend

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -17,6 +17,16 @@ function MyApp() {
       .catch((error) => {
         console.log(error);});
       }, []);
+
+  function postUser(person) {
+    return fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+  }
   
 
   function removeOneCharacter(index) {
@@ -27,7 +37,13 @@ function MyApp() {
   }
 
 function updateList(person){
-  setCharacters([...characters, person]);
+  postUser(person)
+    .then(() => {
+      setCharacters([...characters, person]);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
   return (
